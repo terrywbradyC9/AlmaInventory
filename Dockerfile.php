4c239e057a0edbe50a1aff9e0c7e3ad8f228382a
Terry Brady <terrywbrady@gmail.com>
2018-11-29 13:36:47 -0800

    test coors

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,4 +1,5 @@
 FROM php:7-apache
 COPY webapp/* /var/www/html/
+COPY php/barcodeReport.php /var/www/html/
 COPY php/* /var/www/html/php/
 COPY php/barcode.init.js .
---
+++ php/barcodeReport.php
@@ -0,0 +1,4 @@
+<?php
+    header("Access-Control-Allow-Origin: script.google.com");
+    include("barcodeReport.html");
+?>
